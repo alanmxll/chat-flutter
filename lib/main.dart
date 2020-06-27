a3f0aecfb83c038990c96c704b3ef62bfd41ece8
Alan Maxwell <alanmaxwellantunes@gmail.com>
2020-06-27 20:01:50 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:chat_flutter/app/home/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Chat Flutter',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.yellow,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomePage(),
+    );
+  }
+}
