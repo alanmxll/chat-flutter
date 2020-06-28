b4344dfdcf830823e1864c97b7a5ff20c4ad1020
Alan Maxwell <alanmaxwellantunes@gmail.com>
2020-06-27 21:15:21 -0300

    created a button to navigate to chat page (provisory)

--- lib/app/home/home_page.dart
+++ lib/app/home/home_page.dart
@@ -1,10 +1,39 @@
+import 'package:chat_flutter/app/chat/chat_page.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Chat Flutter'), centerTitle: true),
+      body: Container(
+        child: Center(
+          child: FlatButton(
+            color: Colors.yellow,
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => ChatPage()),
+              );
+            },
+            child: Text(
+              'Chat',
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
